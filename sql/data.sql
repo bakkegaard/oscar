5e7ba3c4d4648af4aa4c8f960715468c29dac5f1
Nick Bakkegaard <nickbakkegaard@gmail.com>
2014-02-28 10:24:13 +0100

    added all categorys

--- sql/data.sql
+++ sql/data.sql
@@ -4,7 +4,33 @@ INSERT INTO film(navn) VALUES ('American Hustle'),('Nebraska'),('The Wolf of Wal
 
 INSERT INTO `guess` VALUES (1,1);
 
-INSERT INTO kategori(navn) VALUES ('Best Actor');
+INSERT INTO kategori(navn) VALUES 
+('Best Picture'),
+('Best Director'),
+('Best Actor'),
+('Best Actress'),
+('Best Supporting Actor'),
+('Best Supporting Actress'),
+('Best Writing - Original Screenplay'),
+('Best Writing - Adapted Screenplay'),
+('Best Animated Feature Film'),
+('Best Foreign Langauge Film'),
+('Best Dcumentary - Feature'),
+('Best Documentary - Short Subject'),
+('Best Live Action Short Film'),
+('Best Animated Short Film'),
+('Best Original Score'),
+('Best Original Song'),
+('Best Sound Editing'),
+('Best Sound Mixing'),
+('Best Production Design'),
+('Best Cinematography'),
+('Best Makeup and Hairstyling'),
+('Best Costume Design'),
+('Best Film Editing'),
+('Best VIsual Effects');
+
+
 
 INSERT INTO `nominering` VALUES (1,1,'Christian Bale','\0',1),(2,1,'Bruce Dern','\0',2),(3,1,'Leonardo DiCaprio','\0',3),(4,1,'Chiwetel Ejiofor','\0',4),(5,1,'Matthew McConaughey','\0',5);
 
