bb7544c68604b2b8ddf2d684ef68d9794f29e263
Nick Bakkegaard <nickbakkegaard@gmail.com>
2014-03-03 00:11:16 +0100

    voting now works

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ var pool = mysql.createPool({
 	database	: config.database
 });
 
-
+String.prototype.replaceAt=function(index, character) {
+	    return this.substr(0, index) + character + this.substr(index+character.length);
+}
 
 http.createServer(function (req, res) {
 
@@ -24,7 +26,50 @@ http.createServer(function (req, res) {
 		});
 	}
 	else if(s[0]==='vote'){
-		makeVotePage(res)
+		makeVotePage(res,s[1])
+	}
+	else if(s[0]=='post'){
+		req.on('data', function (data) {
+			var body='';
+			body +=data;
+
+			var temp= body.substring(5).split("=");
+
+			var hash=temp[0].split("&")[0];
+
+			var numbers= new Array();
+
+			for(var i=1;i<temp.length;i++){
+				numbers.push(parseInt(temp[i]));
+			}
+
+	pool.getConnection(function(err,connection){
+		connection.query("select * from user where hash='"+hash+"'", function(err, rows, fields) {
+				var id= rows[0].id;
+				var query="";	
+				query+= "INSERT INTO guess(user,nominering) VALUES";
+				for(var i=0;i<numbers.length;i++){
+					query+= "('"+id+"','"+numbers[i]+"'),";
+				}
+
+				query= query.replaceAt(query.length-1, ";");
+
+				console.log(query);
+
+				pool.getConnection(function(err,connection){
+					connection.query(query);	
+
+					var page= makePage(res);
+
+					page.write(2,"done!");
+
+					page.toString();
+				});
+
+		});
+		
+	});
+	});
 	}
 	else {
 		res.writeHead(200, {'Content-Type': 'text/html'});
@@ -95,9 +140,11 @@ function makePage(res){
 	return obj;	
 }
 
-function makeVotePage(res){
+function makeVotePage(res,hash){
 
 	var page= makePage(res);
+
+	console.log(hash);
 
 	page.write(0,'<h1>Voting</h1>');
 
@@ -105,14 +152,14 @@ function makeVotePage(res){
 		connection.query("select t1.id,filmnavn,note,navn as kategori from (SELECT nominering.id, navn as filmnavn,note,kategori FROM (film INNER JOIN nominering ON film.id=nominering.film)) as t1 INNER JOIN kategori ON kategori.id=t1.kategori ORDER BY kategori", function(err, rows, fields) {
 	
 		  if (err) throw err;
-		  page.write(1,'<form>');
+		  page.write(1,'<form name="voting" action="/post"method="post">');
 
-
+			page.write(2,'<input type="hidden" name="hash" value="'+hash+'"/>');
 			var i=0, current='lalala';
 			while(i<rows.length){
 
 				if(current==rows[i].kategori){
-					page.write(3,'<input name="'+current+'" type="radio" id="'+rows[i].id+'"/> '+rows[i].filmnavn+' - <b>'+rows[i].note+'</b><br/>');
+					page.write(3,'<input name="'+current+'" type="radio" value="'+rows[i].id+'"/> '+rows[i].filmnavn+' - <b>'+rows[i].note+'</b><br/>');
 				}
 				else{
 					if(i!=0) page.write(4,'</div></div>');
@@ -123,10 +170,12 @@ function makeVotePage(res){
 			page.write(3,'</div>');
 			page.write(3, '<div class="panel-body">');
 
-			page.write(3,'<input name="'+current+'" type="radio" id="'+rows[i].id+'"/> '+rows[i].filmnavn+' - <b>'+rows[i].note+'</b><br/>');
+			page.write(3,'<input name="'+current+'" type="radio" value="'+rows[i].id+'"/> '+rows[i].filmnavn+' - <b>'+rows[i].note+'</b><br/>');
 			}
 		i++;
 			}
+			page.write(4,'</div></div>');
+			page.write(3,'<input type="submit" value="Vote!">');
 		  page.toString();
 		});
 	});
