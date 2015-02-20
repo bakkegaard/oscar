12ca310c27b80601e7604d6f44c28ff54c4f263e
Casper C <casperc91@gmail.com>
2015-02-20 22:20:32 +0100

    Setup framework using jade, setup database

--- server.js
+++ server.js
@@ -2,21 +2,50 @@
 var express = require('express');
 var app = express();
 
+var mysql =  require('mysql');
+var config = require('./config.js')
+
+var pool = mysql.createPool({
+    host     : config.host,
+    user     : config.user,
+    password : config.password,
+    database : config.database
+});
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jade')
+
 //Serve files from public directory
 app.use(express.static(__dirname + '/public'));
 
-//If the file isn't found, send index.htm
-app.get('/',function(req,res){
-	res.redirect('index.htm');
+
+app.get('/', function(req, res){
+	res.render('standings', {
+		title: 'Home',
+        year: config.year
+	});
 });
 
-
-//Serve api
-app.get('/api/', function(req, res){
-	res.send('GET');
+app.get('/nominations', function(req, res){
+    res.render('nominations', {
+        title: 'Home',
+        year: config.year
+    });
+    pool.getConnection(function(err,connection){
+        connection.query("select * from film", function(err, rows, fields) {
+            console.log(rows);
+        });
+    });
 });
 
-//start serves
+app.get('/make_guess', function(req, res){
+    res.render('make_guess', {
+        title: 'Make guess',
+        year: config.year
+    });
+});
+
+//start server
 app.listen(1337);
 
 console.log("server running at localhost:1337");
--- sql/database.sql
+++ sql/database.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS `film`;
 
 CREATE TABLE `film` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `navn` varchar(45) DEFAULT NULL,
+  `navn` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -22,7 +22,7 @@ DROP TABLE IF EXISTS `kategori`;
 
 CREATE TABLE `kategori` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `navn` varchar(45) DEFAULT NULL,
+  `navn` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -32,7 +32,7 @@ DROP TABLE IF EXISTS `nominering`;
 CREATE TABLE `nominering` (
   `film` int(11) DEFAULT NULL,
   `kategori` int(11) DEFAULT NULL,
-  `note` varchar(45) DEFAULT NULL,
+  `note` varchar(200) DEFAULT NULL,
   `winner` int(1) DEFAULT 0,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
